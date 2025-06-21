05e7867ac734d678d41c7b06aefb184b538a5aac
BornToDie4u <lucifer25052005@gmail.com>
2025-06-21 16:55:19 +0530

    mongoChange

--- loginPhoneService/connection.js
+++ loginPhoneService/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 function connectdb() {
-    mongoose.connect("mongodb://127.0.0.1:27017/filmyfi-userPhone-service").then(()=>{
+    // mongoose.connect("mongodb://127.0.0.1:27017/filmyfi-userPhone-service").then(()=>{
+    //     console.log('monog connected');
+    // }) 
+    mongoose.connect("mongodb+srv://lucifer25052005:6gIv65P66Dn6u0fK@cluster0.ajqwve3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
         console.log('monog connected');
     })     
 }
